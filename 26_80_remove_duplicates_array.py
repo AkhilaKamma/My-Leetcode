d24836f477ebc182fe8f226e47745118d7fad1f1
Akhila Kamma <kamma.akhila@gmail.com>
2024-02-17 16:11:54 -0800

    Update 26_80_remove_duplicates_array.py

--- 26_80_remove_duplicates_array.py
+++ 26_80_remove_duplicates_array.py
@@ -26,3 +26,26 @@ class Solution:
                 nums[left], nums[right] = nums[right], nums[left]
 
         return left+1
+
+
+#80... similar style
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+
+        left = right = 0
+
+        while right < len(nums):
+            count = 1
+            while right + 1 < len(nums) and nums[right] == nums[right + 1]:
+                right = right + 1
+                count = count + 1
+
+            for i in range(min(2,count)):
+                nums[left] = nums[right]
+                left = left + 1 
+
+            right = right + 1          
+        return left
+
+
